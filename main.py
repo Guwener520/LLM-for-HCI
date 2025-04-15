ccfc3f8527439ac59db59f3c783d508b1300f00a
ziyu2353730 <3182906081@qq.com>
2025-04-15 20:58:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from openai import OpenAI
 import gradio as gr
 import os
 import json
+import re
 from typing import List, Tuple
 
 
@@ -15,6 +16,9 @@ class Conversation:
         self.num_rounds = max_num_rounds
         self.client = OpenAI(api_key=self.APIkey, base_url=self.BaseUrl)
         self.model = model
+        self.all_mess = {'Pro/Qwen/Qwen2.5-VL-7B-Instruct': [],
+                         'THUDM/chatglm3-6b': [],
+                         'deepseek-ai/DeepSeek-V3': []}
         self.messages = []
         self.messages.append({'role': 'system', 'content': prompt})
         response = self.client.chat.completions.create(
@@ -24,8 +28,10 @@ class Conversation:
         )
 
     def switch_model(self, new_model):
-        self.messages = []
-        self.model = new_model
+        clean_model = process_model_choice(new_model)
+        self.all_mess[self.model] = self.messages
+        self.messages = self.all_mess[clean_model]
+        self.model = clean_model
         return []
 
     def interact_chat(self, chatbot: List[Tuple[str, str]], user_input: str,
@@ -36,11 +42,7 @@ class Conversation:
           - temp: 模型的温度参数。温度用于控制聊天机器人的输出。温度越高，响应越具创造性。
         """
         try:
-            messages = []
-            for input_text, response_text in chatbot:
-                messages.append({'role': 'user', 'content': input_text})
-                messages.append({'role': 'assistant', 'content': response_text})
-
+            messages = self.messages
             messages.append({'role': 'user', 'content': user_input})
 
             response = self.client.chat.completions.create(
@@ -48,7 +50,10 @@ class Conversation:
                 messages=messages,  # 包含用户的输入和对话历史
                 temperature=temp,  # 使用温度参数控制创造性
             )
+            messages.append({'role': 'assistant',
+                             'content': response.choices[0].message.content})
             chatbot.append((user_input, response.choices[0].message.content))
+            self.messages = messages
 
         except Exception as e:
             print(f"发生错误：{e}")
@@ -56,6 +61,10 @@ class Conversation:
         return chatbot, ""
 
     def reset(self):
+        self.all_mess['Pro/Qwen/Qwen2.5-VL-7B-Instruct'] = []
+        self.all_mess['THUDM/chatglm3-6b'] = []
+        self.all_mess['deepseek-ai/DeepSeek-V3'] = []
+
         self.messages = []
         return []
 
@@ -65,7 +74,14 @@ class Conversation:
           - model: 选择的模型
           - messages: 对话记录
         """
-        target = {"model": self.model,  "description": description}
+        target = [{"model": 'Qwen2.5-VL-7B-Instruct',
+                   "description": self.all_mess[
+                       'Pro/Qwen/Qwen2.5-VL-7B-Instruct']},
+                  {"model": 'chatglm3-6b',
+                   "description": self.all_mess['THUDM/chatglm3-6b']},
+                  {"model": 'deepseek-ai/DeepSeek-V3',
+                   "description": self.all_mess[
+                       'deepseek-ai/DeepSeek-V3']}]
         file_path = 'files/dialogue_history.json'
         directory = os.path.dirname(file_path)
         if not os.path.exists(directory):
@@ -79,16 +95,26 @@ class Conversation:
             print(f"导出对话时发生错误：{e}")
 
 
+def process_model_choice(selected_model):
+    pattern = r'#(.*?)#'
+    matches = re.findall(pattern, selected_model)
+    selected_model = selected_model.replace(f'#{matches[0]}#', '')
+    return selected_model
+
+
 if __name__ == '__main__':
     character_for_chatbot = 'assistant'
     prompt_for_dialogue = "你是一个有用的AI助手，接下来请回答我的问题"
 
     model1 = 'Pro/Qwen/Qwen2.5-VL-7B-Instruct'
-    model2 = 'deepseek-ai/DeepSeek-V3'
-    model3 = 'deepseek-ai/DeepSeek-R1-Distill-Qwen-7B'
-    model4 = 'THUDM/glm-4-9b-chat'
+    model2 = 'THUDM/chatglm3-6b'
+    model3 = 'deepseek-ai/DeepSeek-V3'
 
-    models = [model1, model2, model3, model4]
+    models = [model1, model2, model3]
+
+    model_description = [model1 + '#全面的AI助手，善于推理、编程#',
+                         model2 + '#开源模型，免费调用，对话流畅#',
+                         model3 + '#深度思考，回答准确，反应较慢#']
 
     conversation = Conversation(model1, prompt_for_dialogue)
 
@@ -100,25 +126,23 @@ if __name__ == '__main__':
         gr.Markdown(
             f"# 我是你的AI助手，试着问我一些问题！")
         chatbot = gr.Chatbot(value=first_dialogue[0])
-        description_textbox = gr.Textbox(label="机器人扮演的角色",
-                                         interactive=False,
-                                         value=f"{character_for_chatbot}")
         input_textbox = gr.Textbox(label="输入", value="")
+
+        with gr.Row():
+            sent_button = gr.Button(value="发送")
+            reset_button = gr.Button(value="重置")
 
         with gr.Column():
             gr.Markdown("# 选择模型")
-            model_dropdown = gr.Dropdown(choices=models,
-                                         label="选择模型", value=model1)
+            model_dropdown = gr.Dropdown(choices=model_description,
+                                         label="选择模型",
+                                         value=model_description[0])
 
         with gr.Column():
             gr.Markdown(
                 "# 温度调节\n温度用于控制聊天机器人的输出。温度越高，响应越具创造性。")
             temperature_slider = gr.Slider(0.0, 1.9, 1.0, step=0.1,
                                            label="温度")
-
-        with gr.Row():
-            sent_button = gr.Button(value="发送")
-            reset_button = gr.Button(value="重置")
 
         with gr.Column():
             gr.Markdown("# 保存结果\n当你对结果满意后，点击导出按钮保存结果。")
